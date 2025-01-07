15f2b2d2fab30789595297e851a78d847af74764
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-01-07 23:34:42 +0700

    layout commit

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/vn.svg" 
+                        alt="Vietnamese" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md" 
+                    />
+                    Vietnamese
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/uk.svg" 
+                        alt="England" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md" 
+                    />
+                    England
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/jp.svg" 
+                        alt="Japanese" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md" 
+                    />
+                    Japanese
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/sw.svg" 
+                        alt="Sweden" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md" 
+                    />
+                    Sweden
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/ca.svg" 
+                        alt="Canadian" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md" 
+                    />
+                    Canadian
+                </Button>
+            </div>
         </footer>
     );
 };
