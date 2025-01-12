66a7f3b840835ed97b9b533b6a9b57811e350134
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-01-12 09:33:12 +0700

    get and display db related to getUserProgress from /db/queries

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,21 +1,37 @@
+import { redirect } from "next/navigation";
+
+import { getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
 
-const LearnPage = () => {
+import { Header } from "./header";
+
+const LearnPage = async () => {
+    const userProgressData = getUserProgress();
+
+    const [
+        userProgress
+    ] = await Promise.all([
+        userProgressData
+    ]);
+
+    if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    }
+
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress 
-                    activeCourse={{ title: "English", imageSrc: "/uk.svg" }}
-                    hearts={5}
-                    points={100}
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false}
                 />
             </StickyWrapper>
             <FeedWrapper>
-                <Header title="English" />
+                <Header title={userProgress.activeCourse.title} />
             </FeedWrapper>
         </div>
     );
