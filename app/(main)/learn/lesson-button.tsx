1e53a1a96aeb2a857f250a38c1dcf826e7fdbf82
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-02-19 00:05:14 +0700

    adjust color of circularProgressBar

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -76,7 +76,7 @@ export const LessonButton = ({
                             value={Number.isNaN(percentage) ? 0 : percentage}
                             styles={{
                                 path: {
-                                    stroke: "#4ade80",
+                                    stroke: "#ff4d4d",
                                 },
                                 trail: {
                                     stroke: "#e5e7eb",
