4c2827d3521154eaa147ce40abbc20619c46810f
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-02-27 23:56:07 +0700

    add progress-reset script

---
+++ scripts/progress-reset.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+    try {
+        console.log("Resetting progress");
+
+        await db.delete(schema.userProgress);       
+        await db.delete(schema.challengeProgress);
+
+        console.log("Resetting progress finished");
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to reset the progress");
+    }
+};
+
+main();
