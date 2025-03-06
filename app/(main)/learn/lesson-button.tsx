bdac02072e03c6c708dd133b4fe8ab3379e2cc35
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-03-06 15:31:17 +0700

    redesign all the lesson button to make it more beautiful (change shape, posision, size)

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -61,12 +61,12 @@ export const LessonButton = ({
                 className="relative"
                 style={{
                     right: `${rightPosition}px`,
-                    marginTop: isFirst && !isCompleted ? 60 : 24,
+                    marginTop: isFirst && !isCompleted ? 80 : 32,
                 }}
             >
                 {current ? (
-                    <div className="h-[102px] w-[102px] relative">
-                        <div className="absolute -top-6 left-2.3 px-3 py-2.5 border-2 font-bold uppercase text-red-500 bg-white rounded-xl animate-bounce tracking-wide z-10 whitespace-nowrap">
+                    <div className="h-[130px] w-[130px] relative">
+                        <div className="absolute -top-6 left-3.5 px-3 py-2.5 border-2 font-bold uppercase text-red-500 bg-white rounded-xl animate-bounce tracking-wide z-10 whitespace-nowrap">
                             Bắt Đầu
                             <div
                                 className="absolute left-1/2 -bottom-2 w-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2"                           
@@ -86,17 +86,16 @@ export const LessonButton = ({
                             <Button
                                 size="rounded"
                                 variant={locked ? "locked" : "secondary"}
-                                className="h-[70px] w-[70px] border-b-8"
+                                className="h-[90px] w-[90px] border-b-8"
                             >
                                 <Icon
                                     className={cn(
-                                        //"h-[40px] w-[40px]",
                                         locked
                                         ? "fill-neutral-400 text-neutral-400 stroke-neutral-400" 
                                         : "fill-primary-foreground text-primary-foreground",
                                         isCompleted && "fill-none stroke-[4]"
                                     )}
-                                    style={{ height: '40px', width: '40px' }}
+                                    style={{ height: '60px', width: '60px' }}
                                 />
                             </Button>
                         </CircularProgressbarWithChildren>
@@ -105,17 +104,16 @@ export const LessonButton = ({
                     <Button
                         size="rounded"
                         variant={locked ? "locked" : "secondary"}
-                        className="h-[70px] w-[70px] border-b-8"
+                        className="h-[90px] w-[200px] border-b-8"
                     >
                         <Icon
                             className={cn(
-                                //"h-[40px] w-[40px]",
                                 locked
                                 ? "fill-neutral-400 text-neutral-400 stroke-neutral-400" 
                                 : "fill-primary-foreground text-primary-foreground",
                                 isCompleted && "fill-none stroke-[4]"
                             )}
-                            style={{ height: '40px', width: '40px' }}
+                            style={{ height: '70px', width: '70px' }}
                         />
                     </Button>
                 )}
