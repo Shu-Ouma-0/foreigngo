9e27d4b799164b1fd4c73094904c2d8a8563270b
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-03-06 15:01:24 +0700

    add new button and adjust the color of some components (sidebar and sidebarOutline)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,6 +13,7 @@ const buttonVariants = cva(
         default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
         primary: "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
         primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
+        primaryOutline2: "bg-transparent text-sky-300 hover:bg-white",
         secondary: "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-red-700 border-b-4 active:border-b-0",
         third: "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-700 border-b-4 active:border-b-0",
         secondaryOutline: "bg-white text-green-500 hover:bg-slate-100",
@@ -21,8 +22,9 @@ const buttonVariants = cva(
         super: "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
         superOutline: "bg-white text-indigo-500 hover:bg-slate-100",
         ghost: "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
-        sidebar: "bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-none",
-        sidebarOutline: "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none"
+        ghost2: "bg-transparent text-red-900 border-transparent border-0 hover:bg-slate-100",
+        sidebar: "bg-transparent text-red-900 border-2 border-transparent hover:bg-slate-100 transition-none",
+        sidebarOutline: "bg-red-900/30 text-pink-100 border-red-500 border-2 hover:bg-red-500/30 transition-none"
       },
       size: {
         default: "h-11 px-4 py-2",
