59a8e37b7137c734032f6bf3d9409b84c8452224
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-03-06 15:26:58 +0700

    add picture to header, try to make the color match with the color-bg of learn page (took a lot of my time to figure it out T_T), change color in bg and text.

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -8,13 +8,16 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
     return (
-        <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
+        <div className="sticky top-0 bg-gradient-to-r from-red-300 via-red-200 to-[#c8777f] pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
+
+        <div className="absolute left-1/2 top-1/2 w-[70%] h-[70%] bg-[url('/header-bg.svg')] bg-cover bg-center rounded-lg -translate-x-1/2 -translate-y-1/2 -z-10" />
+
             <Link href="/courses">
                 <Button variant="ghost" size="sm">
-                    <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
+                    <ArrowLeft className="h-5 w-5 stroke-2 text-red-900" />
                 </Button>
             </Link>
-            <h1 className="font-bold text-lg">
+            <h1 className="font-bold text-lg text-white px-1 bg-rose-900 rounded-md shadow-md -translate-y-2">
                 {title}
             </h1>
             <div />
